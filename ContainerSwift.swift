f8045a8276e60b455dbfe6662597d06a84bbbf7f
eduardofulgenciocomendeiro <eduardofulgenciocomendeiro@gmail.com>
2022-02-11 15:46:22 +0100

    Create class ContainerSwift
    With TriviaAPI method load.

---
+++ ContainerSwift.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaAPI.swift
+//  MovieTrivia
+//
+//  Created by Donny Wals on 16/10/2016.
+//  Copyright © 2016 DonnyWals. All rights reserved.
+//
+
+import Foundation
+
+struct TriviaAPI: TriviaAPIProviding {
+    
+    func loadTriviaQuestions() -> String {
+      return "Ok"
+    }
+    
+} 
